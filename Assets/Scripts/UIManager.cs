54513ebff551be5564d5e0aa87c1908ad3cfe79d
SMC <tyuui88@gmail.com>
2025-08-27 11:39:36 +0900

    feat : 임금,출금,송금 UI 생성 및 스크립트 추가

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public GameObject mainUI;
+    public GameObject depositUI;
+    public GameObject withDrawUI;
+    public GameObject sendUI;
+
     public TextMeshProUGUI userName;
     public TextMeshProUGUI gold;
     public TextMeshProUGUI bankGold;
@@ -10,6 +15,7 @@ public class UIManager : MonoBehaviour
     public void Start()
     {
         Refresh();
+        GotoMain();
     }
 
     public void Refresh()
@@ -17,5 +23,36 @@ public class UIManager : MonoBehaviour
         userName.text = GameManager.Instance.userData.userName;
         gold.text = GameManager.Instance.userData.userGold.ToString();
         bankGold.text = GameManager.Instance.userData.userBankGold.ToString();
+    }
+
+    public void GotoMain()
+    {
+        mainUI.SetActive(true);
+        depositUI.SetActive(false);
+        withDrawUI.SetActive(false);
+        sendUI.SetActive(false);
+    }
+
+    public void GotoDepositUI()
+    {
+        mainUI.SetActive(false);
+        depositUI.SetActive(true);
+        withDrawUI.SetActive(false);
+        sendUI.SetActive(false);
+    }
+    public void GotoWithDrawUI()
+    {
+        mainUI.SetActive(false);
+        depositUI.SetActive(false);
+        withDrawUI.SetActive(true);
+        sendUI.SetActive(false);
+    }
+
+    public void GotoSendUI()
+    {
+        mainUI.SetActive(false);
+        depositUI.SetActive(false);
+        withDrawUI.SetActive(false);
+        sendUI.SetActive(true);
     }
 }
